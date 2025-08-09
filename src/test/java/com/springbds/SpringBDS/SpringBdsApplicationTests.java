7c4f3d61fd0ea7cf24fadcbdee87719bd99aac6c
yamil <moselli.yamil.1a@gmail.com>
2025-08-09 02:52:14 -0300

    initializr

---
+++ src/test/java/com/springbds/SpringBDS/SpringBdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbds.SpringBDS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
