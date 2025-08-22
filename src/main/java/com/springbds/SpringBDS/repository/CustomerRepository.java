cab030a00ceba188757e434089e720e6fd46fd28
yamil <moselli.yamil.1a@gmail.com>
2025-08-22 11:33:51 -0300

    JPQL queries and native queries on CustomerRepository

--- src/main/java/com/springbds/SpringBDS/repository/CustomerRepository.java
+++ src/main/java/com/springbds/SpringBDS/repository/CustomerRepository.java
@@ -1,7 +1,9 @@
 package com.springbds.SpringBDS.repository;
 
 import com.springbds.SpringBDS.entity.Customer;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +12,17 @@ import java.util.Optional;
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     Optional<Customer> findByFirstName(String firstName);
-    // List<Customer> findByFirstNameContaining(String first_name);
+    List<Customer> findByFirstNameContaining(String firstName);
+    List<Customer> findByCountry(String country);
 
+    // JPQL query
+    @Query("SELECT c.firstName FROM Customer c WHERE c.dni=?1")
+    String getFirstNameByDni(String dni);
+
+    // Native query
+    @Query(
+            value = "SELECT country FROM customers WHERE dni=?1",
+            nativeQuery = true
+    )
+    String getCountryByDni(String dni);
 }
--- src/test/java/com/springbds/SpringBDS/repository/CustomerRepositoryTest.java
+++ src/test/java/com/springbds/SpringBDS/repository/CustomerRepositoryTest.java
@@ -1,9 +1,14 @@
 package com.springbds.SpringBDS.repository;
 
 import com.springbds.SpringBDS.entity.Customer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,19 +27,79 @@ class CustomerRepositoryTest {
 
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private TestEntityManager testEntityManager;
 
-    @Test
-    public void saveCustomerTest() {
-        Customer c1 = Customer.builder()
+    private Customer savedC1;
+    private Customer c2;
+    private Customer c3;
+    private Customer c4;
+
+    @BeforeEach
+    void setUp(){
+        savedC1 = Customer.builder()
                 .firstName("Carlos")
                 .lastName("Perez")
                 .country("Argentina")
                 .dni("12345678")
                 .build();
 
-        Customer savedCustomer = customerRepository.save(c1);
-        assertEquals("Carlos", savedCustomer.getFirstName());
-        assertNotNull(savedCustomer.getId());
+        c2 = Customer.builder()
+                .firstName("Juan")
+                .lastName("Moselli")
+                .country("Argentina")
+                .dni("23789456")
+                .build();
+
+        c3 = Customer.builder()
+                .firstName("Camilo")
+                .lastName("Gonzalez")
+                .country("Peru")
+                .dni("23784203")
+                .build();
+
+        c4 = Customer.builder()
+                .firstName("Juan")
+                .lastName("Gonzalez")
+                .country("Chile")
+                .dni("23784206")
+                .build();
+
+        testEntityManager.persist(savedC1);
+        testEntityManager.persist(c2);
+        testEntityManager.persist(c3);
+        testEntityManager.persist(c4);
+    }
+
+    @Test
+    public void saveCustomerTest() {
+
+        assertEquals("Carlos", savedC1.getFirstName());
+        assertNotNull(savedC1.getId());
+    }
+
+    @Test
+    public void findByFirstNameContainingTest() {
+        List<Customer> lista = customerRepository.findByFirstNameContaining("il");
+        System.out.println(lista);
+    }
+
+    @Test
+    public void findByCountryTest(){
+        List<Customer> lista = customerRepository.findByCountry("Argentina");
+        System.out.println(lista);
+    }
+
+    @Test
+    public void getFirstNameByDniTest(){
+        String first_name = customerRepository.getFirstNameByDni("23784206");
+        System.out.println("firstName = " + first_name);
+    }
+
+    @Test
+    public void getCountryByDniTest(){
+        String returnedCountry = customerRepository.getCountryByDni("23784206");
+        System.out.println("Country = " + returnedCountry);
     }
 
 }
